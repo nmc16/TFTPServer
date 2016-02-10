af3a51291371c16097d2332d3c1030f8b7462b26
KevinRMiller93 <kevinrmiller@carleton.cmail.com>
2016-02-10 17:05:06 -0500

    basic Error Simulation

--- Iteration2/error/ErrorSimThread.java
+++ Iteration2/error/ErrorSimThread.java
@@ -3,8 +3,11 @@ package error;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import shared.Helper;
 /**
@@ -26,7 +29,55 @@ public class ErrorSimThread implements Runnable {
 			se.printStackTrace();
 			System.exit(1);
 		} 
-	}	
+	}
+	
+	
+	public static DatagramPacket BringError(byte msg[], String ErrCode, DatagramPacket Received, int porter) {
+        byte newmsg[] = msg;
+        InetAddress address = Received.getAddress();
+        int length = Received.getLength();
+        int port = porter;
+        
+        try{
+        	address = InetAddress.getByName("127.0.0.1");
+        } catch (UnknownHostException e) {
+            System.out.println("Could not stop requests properly: " + e.getMessage());
+            e.printStackTrace();
+        }
+        
+        
+        if(ErrCode.equals("01")){
+        	//mess with data byte 1
+        	newmsg[1] = 6;
+        } else if(ErrCode.equals("02")){
+        	//mess with data byte 0
+        	newmsg[0] = 6;
+        } else if(ErrCode.equals("03")){
+        	//mess with port
+        	port = 1;
+        } else if(ErrCode.equals("04")){
+        	//Wrong Address
+        	try{
+            	address = InetAddress.getByName("127.0.0.1");
+            } catch (UnknownHostException e) {
+                System.out.println("Could not stop requests properly: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        return new DatagramPacket(newmsg, length, address, port);
+    }
+	
+	public void PrintErrorList(){
+		System.out.print("\n\nThis is the error SIM. To choose your error, please print exactly what's between the quotations: \n");
+		System.out.print("\"00\": Normal Operations\n");
+		System.out.print("\"01\": Changes the first byte in the OpCode\n");
+		System.out.print("\"02\": Changes the second byte in the OpCode\n");
+		System.out.print("\"03\": Change to an invalid port number\n");
+		System.out.print("\"04\": Change to a different Address\n");
+	    System.out.print("> ");
+	}
+	
+	
 	
 	/**
 	 *    
@@ -45,6 +96,8 @@ public class ErrorSimThread implements Runnable {
 	@Override
 	public void run() {
 		// Initialize the variables
+		  Scanner reader = new Scanner(System.in);
+		  String input;
 	      byte data[] = new byte[516];
 	      
 	      //create the packet
@@ -99,11 +152,15 @@ public class ErrorSimThread implements Runnable {
 		      System.out.println("Length: " + receivePacket.getLength());
 		      System.out.println("Containing: " + new String(receivePacket.getData()));
 		      System.out.println("In bytes " + Arrays.toString(dataminr) + "\n\n");
+		      
+		      
+		      //Decide on the error sim
+		      PrintErrorList();
+	          input = reader.nextLine();
 		
 		      
 		      //create a new packet to send
-		      sendPacket = new DatagramPacket(data, receivePacket.getLength(),
-		              						  receivePacket.getAddress(), clientPort);
+		      sendPacket = BringError(dataminr, input, receivePacket, clientPort);
 		      
 		      byte datamins2[] = Helper.minimi(sendPacket.getData(), sendPacket.getLength());
 		      //print out the data to be sent
@@ -141,9 +198,14 @@ public class ErrorSimThread implements Runnable {
 		    	  System.exit(1);
 		      }
 		      
+		      
+		    //Decide on the error sim
+		      PrintErrorList();
+	          input = reader.nextLine();
+		
+		      
 		      //create a new packet to send
-		      sendPacket = new DatagramPacket(receivePacket.getData(), receivePacket.getLength(),
-		              						  receivePacket.getAddress(), serverPort);
+		      sendPacket = BringError(data, input, receivePacket, serverPort);
 		      
 		      byte datamins3[] = Helper.minimi(sendPacket.getData(), sendPacket.getLength());
 		      //print out the data to be sent
@@ -165,7 +227,8 @@ public class ErrorSimThread implements Runnable {
 	      
 
 	      // We're finished, so close the sockets.
-	      sendReceiveSocket.close();		
+	      sendReceiveSocket.close();
+	      reader.close();
 	}
 	
 }
